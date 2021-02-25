c01a931c9d9be00308382e0ddd86ad654117e119
daiywlf@gmail.com <daiywlf5251@gmail.com>
2021-02-25 16:35:35 +0900

    update date form using momentjs package

--- nodejsEx/sequelize/models/Products.js
+++ nodejsEx/sequelize/models/Products.js
@@ -1,3 +1,5 @@
+const moment = require('moment')
+
 module.exports = function(sequelize, DataTypes){
     const Products = sequelize.define('Products',
         {
@@ -7,5 +9,12 @@ module.exports = function(sequelize, DataTypes){
             description : { type: DataTypes.TEXT }
         }
     );
+
+    Products.prototype.dateFormat = (date) => (
+        moment(date).format('YYYY-MM-DD')
+    )
+    // 자바스크립트 날짜 형태를 받아서 년월일로 만들어주는 함수
+    // return없애고  함수를 소괄호 사용해서 바로 리턴을 해주는 형식
+    
     return Products;
 } //테이블 구성 요소
